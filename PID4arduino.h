325e697996f3fe0cafea3087fd585b9bf2de05e8
AKIRA2390 <ando--akira@outlook.com>
2021-10-20 00:36:29 +0900

    いろいろと修正しました

--- PID4arduino.h
+++ PID4arduino.h
@@ -1,35 +1,39 @@
 #include <Arduino.h>
 
-// intまたはdoubleを想定
-template <typename T>
+namespace PID4Arduino {
+typedef struct PIDGain_t {
+  double KP = 0, KI = 0, KD = 0;
+};
+
+template <typename T>  // intまたはdoubleを想定
 class PID4arduino {
  private:
-  double kp = 0, ki = 0, kd = 0;
-  T P = 0, I = 0, D = 0,PID = 0;
-  T prevPV;
-  unsigned long prevTime;
+  PIDGain_t PIDGains;
+  T P = 0, I = 0, D = 0, PID = 0;
+  T prevPV = 0;
+  unsigned long prevTime = 0;
 
  public:
   PID4arduino() {}
   ~PID4arduino() {}
 
-  void setGains(double kp, double ki, double kd) {
-    this->kp = kp;
-    this->ki = ki;
-    this->kd = kd;
-  }
-  // SVが目標値、PVが測定値、MVが出力
-  void update(T SV, T PV, unsigned long now) {
-    T MV, error = SV - PV;
-    unsigned long dt = (now - prevTime);
-    P = error * kp;
-    I += dt * error;
-    D = error / dt;
+  void setGains(PIDGain_t Gains) { memcpy(PIDGains, Gains, syzeof(Gains)); }
+
+  // SVが目標値、PVが測定値
+  void update(T SV, T PV) {
+    T error = SV - PV;
+    unsigned long now = millis(), dt = now - prevTime;
+
+    P = error * PIDGains.KP;
+    I += dt * error * PIDGains.KI;
+    D = error / dt * PIDGains.KD;
+
+    PID = P + I + D;
 
     prevTime = now;
     prevPV = PV;
-    PID = P + I + D;
   }
 
-  T write() { return PID; }
+  T GetValue() { return PID; }
 };
+}  // namespace PID4Arduino
--- readme.md
+++ readme.md
@@ -1,17 +1,18 @@
 # PID制御ライブラリ
-PID制御を完全に理解したので書いてみた  
+~~PID制御を完全に理解したので書いてみた  
 "4arduino"とあるように、arduinoで使うために書いた  
 だけど、arduinoに依存するものは使ってない、ので別にarduino専用ってわけでもない  
 ちなみにtemprateを初めて使ってみた  
-temprateも完全に理解した
-
+temprateも完全に理解した~~
+いろいろとアレだったので修正しました
+当時はmbedとの互換性を考えてたみたいだけど、普通にarduinoに依存してるので捨てました
 
 # 使い方
 
 
 
 ## 宣言
-```cpp:実はまだ試していないので本当にこれでいいのかはわからない.cpp
+```cpp:実はまだ試していないので本当にこれでいいのかはわからない_テストくらいしなさい.cpp
  PID4arduino pid = PID4arduino<int>;
  PID4arduino pid = PID4arduino<double>;
 
@@ -21,28 +22,37 @@ intやdouble,floatを使って宣言できる
 
 ## ゲイン設定
 ```cpp:hogehoge.cpp
-pid.setGains(kp,ki,kd);
+PID4Arduino::PIDGain_t Gains;
+
+Gains.KP = P_Gain;
+Gains.KI = I_Gain;
+Gains.KD = D_Gain
+
+pid.setGains(Gains);
 ``` 
 これを忘れるとゲインはすべてゼロになる  
 ## 入力
 ```cpp:fugafuga.cpp
-update(int SV, int PV, millis());
-update(double SV, double PV, millis());
+update(int SV, int PV);
+update(double SV, double PV);
 ```
 SVが目標値、PVが測定値
 
 ## 出力
 ```cpp:piyopiyo.cpp
-analogWrite(pin,pid.write());
+analogWrite(pin,pid.GetValue());
 ```
 
 
 # info
 ## 書いた人
- **†*akira2390*†**
+ akira2390
 
 ## 書き始めた日
  2020/12/16
 
 ## 書き終わった日
  2020/12/16
+
+## 修正し始めた日
+ 2020/10/20
